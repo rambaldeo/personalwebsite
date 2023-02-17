179676ed04850a999d78961035d294498d2e9fc3
Ram Baldeo <77577818+rambaldeo@users.noreply.github.com>
2023-02-17 16:02:34 -0500

    Add files via upload

---
+++ bootstrap-3.3.7-dist/Resume portfolio.php
@@ -0,0 +1,130 @@
+<? php
+echo ' hello';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>
+Ram Baldeo's Personal Website
+</title>
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Tangerine:700' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<div id="topheader" class="navbar navbar-detault navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-coolapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+
+			</button>
+			<a class="navbar-brand" href="#">Personal Website</a>
+
+		</div>
+
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="active"> <a href="Resume portfolio.html"> Home </a> </li>
+				<li class="active"> <a href="Qualifications.html"> Qualifications </a></li>
+				<li class="active"> <a href="Experience.html"> Experience </a></li>
+
+			</ul>
+		</div>
+	</div>
+</div> <!-- This is topheader -->
+
+
+<br>
+<div id="Identification">
+<link rel="stylesheet" href="center.css">
+
+
+<h1>Portfolio of</h1>
+<h1>Ram Baldeo</h1>
+<br>
+<div id="Contact">
+ 	<img src="/Users/Ram/Desktop/Jobs/bootstrap-3.3.7-dist/Photos/IMG_7369.jpeg" class="center" height="400"border="50">
+ </div>
+<br>
+<br>
+<p>Ideas for this project. redo introduction page, create a qualification page, goal and dreams page, interest page 
+
+	</p>
+
+<br>
+<div id="about">
+<h2>About</h2>
+
+<p>
+	Growing up, I've always been fancinated by computers and wanted to create my own application in the future. I was never the smartest kid in the room, but I try to be as observant as possible to notice the little details. Entering high school, I knew I wanted to be involved somewhere in the computer science field and I decided to go with software development. That was my goal in highschool, to get accepted into a university for a software related program because everything is going to involve software somehow and also, because I want to create my own application.
+	</p>
+</div>
+<!--
+<div id="about2">
+	<p>Well lets see how this goes
+	</p>
+
+	</div> -->				<!--about 2 -->
+
+
+
+<!--	 <div class="split left">
+ 	<div class="centered">
+ 		<p>
+ 			This is trial to see if it splits in half
+ 		</p>
+ 	</div>
+ </div>
+ this splits the whole page in half
+ -->
+
+
+ <div class="row">
+ 	<div class="columm">
+ 		<p> Growing up, I was always a very athletic child. I love playing sports, trying new challenges and creating different things. My main sport is soccer but my main subject in school was math. It is always something I come to understand very easily despite being given the most complicated equations. Some of the equations are difficult to solve but with the right preparation and discipline, it is able to solve. </p>
+ 	</div>
+ 	<div class="column">
+ 		
+ 		<img src="/Users/Ram/Desktop/Jobs/bootstrap-3.3.7-dist/Photos/IMG_5318.jpeg" height="350">
+ 		
+ 	</div>
+ </div> <!--This code works to split a specific section of the page in half -->
+
+ <h2>Contact Information</h2>
+<table>
+	<tr>
+	<th>Personal Email: &nbsp;&nbsp; </th>
+	<th>rambaldeo241@gmail.com</th>
+<tr>
+	<th>School Email: &nbsp;&nbsp;</th>
+	<th>ramkumarjr.baldeo@ontariotechu.net </th>
+	</tr>
+<tr>
+	<th>Phone Number: &nbsp;&nbsp;</th>
+	<th>416-877-2732</th>
+</tr>
+
+</table>
+
+</div> <!--this is for identification div -->
+<br>
+<br>
+<br>
+</body>
+
+</html>
